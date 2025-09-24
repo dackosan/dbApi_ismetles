236d0aef7a43755a83917a9b12a36101c6e8fc53
Gyorgy David <gyorgy.david2006@gmail.com>
2025-09-24 12:15:15 +0200

    mukodik

--- util/database.js
+++ util/database.js
@@ -11,9 +11,12 @@ export const getCarById = (id) =>
   db.prepare("SELECT * FROM cars WHERE id = ?").get(id);
 export const createCar = (name, brand) =>
   db.prepare("INSERT INTO cars (name, brand) VALUES (?,?)").run(name, brand);
-export const updateCar = () => db.prepare("UPDATE").run();
+export const updateCar = (id, name, brand) =>
+  db
+    .prepare("UPDATE cars SET name = ?, brand = ? WHERE id = ?")
+    .run(name, brand, id);
 export const deleteCar = (id) =>
-  db.prepare("DELETE * FROM cars WHERE id = ?").run(id);
+  db.prepare("DELETE FROM cars WHERE id = ?").run(id);
 
 //if (!db.getCars) {
 //  createCar("Lada", "4x4");
